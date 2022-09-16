75ee15428c91838a77ee9d117c90bc8f8f66a824
Jefferson Eloy <contatojeloydev@gmail.com>
2022-09-15 22:43:46 -0300

    feat: added example route to list ads

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.send('Hello World')
+app.get('/ads', (request, response) => {
+  return response.send([
+    { id: 1, name: 'Anúncio 1'},
+    { id: 2, name: 'Anúncio 2'},
+    { id: 3, name: 'Anúncio 3'}
+  ])
 })
 
 app.listen(3333)
