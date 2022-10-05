44cd897d8f99ea3f700e533e07e1721bd5f73e4f
Jefferson Eloy <contatojeloydev@gmail.com>
2022-10-04 23:46:11 -0300

    feat: added cards games in home page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89deg, #9572fc 0%, #43e7ad 56%, #e1d55d 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    },
   },
   plugins: [],
 }
