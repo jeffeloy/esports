9eaef56077abf03839115454251b5a0a467a5315
Jefferson Eloy <contatojeloydev@gmail.com>
2022-10-06 23:52:51 -0300

    feat: added font Inter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -65,7 +65,7 @@ function App() {
       <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg  mt-8 overflow-hidden ">
         <div className="bg-[#2A2634] px-8 py-6 self-stretch flex justify-between items-center">
           <div>
-            <strong className="text-2xl text-white font-black">Não encotrou seu duo?</strong>
+            <strong className="text-2xl text-white font-black">Não encontrou seu duo?</strong>
             <span className="text-zinc-400 block">Publique um anúncio para novos players!</span>
           </div>
 
